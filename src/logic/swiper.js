09c4a3f2e7a1a3de00a036c05e3d3e5a0d0b329e
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2019-01-05 00:00:26 +0900

    docs追加忘れ

--- src/logic/swiper.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const swiper = new Swiper('.swiper-container', {
-    pagination: '.swiper-pagination',
-    paginationClickable: true,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    loop: true
-  })
-}, false)
